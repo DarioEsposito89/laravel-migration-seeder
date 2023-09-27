ca534fefcddeaaa90dab2eda813faaad992bdc16
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-27 18:26:06 +0200

    creato struttura per stampare elementi di Train

---
+++ resources/views/card.blade.php
@@ -0,0 +1,43 @@
+@extends("layouts.public")
+
+@section("content")
+<div class="container">
+    <div class="row">
+        <div class="col p-3">
+            <div class="bg-warning">
+            <h1 class="text-center text-primary border border-warning p-3">Orari Treni Partenze/Arrivi</h1>
+        </div>
+            <table class="table table-warning table-striped">
+                <thead>
+                    <tr>
+                        <th>Azienda</th>
+                        <th>Stazione di partenza</th>
+                        <th>Stazione di arrivo</th>
+                        <th>Orario di partenza</th>
+                        <th>Orario di arrivo</th>
+                        <th>Codice treno</th>
+                        <th>Numero carrozze</th>
+                        <th>Treno in orario</th>
+                        <th>Treno cancellato</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($trains as $train)
+                    <tr>
+                        <td>{{$train['Azienda']}}</td>
+                        <td>{{$train['stazione_di_partenza']}}</td>
+                        <td>{{$train['stazione_di_arrivo']}}</td>
+                        <td>{{$train['orario_di_partenza']}}</td>
+                        <td>{{$train['orario_di_arrivo']}}</td>
+                        <td>{{$train['codice_treno']}}</td>
+                        <td>{{$train['numero_carrozze']}}</td>
+                        <td>{{$train['treno_in_orario']}}</td>
+                        <td>{{$train['treno_cancellato']}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection

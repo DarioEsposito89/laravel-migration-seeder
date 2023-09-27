e992e67999d9b3dec7ac2446477acb1256e86764
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-27 18:22:00 +0200

    creato tabella con migration

---
+++ database/migrations/2023_09_27_153532_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 45);
+            $table->string('stazione_di_arrivo', 45);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno', 15);
+            $table->smallInteger('numero_carrozze');
+            $table->boolean('treno_in_orario')->unsigned();
+            $table->boolean('treno_cancellato')->unsigned();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
